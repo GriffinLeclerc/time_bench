6e64c5f5ef29544f903d50e27938000c4aaf1fc8
griffin <leclercgriffin@gmail.com>
2022-04-20 11:52:15 -0400

    master refactor to use async writes

--- gather_measurments.sh
+++ gather_measurments.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-for i in {1..300000}
-do
-   ./target/release/timestamp_benchmark
-done
+./target/release/timestamp_benchmark
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,52 @@
-extern crate time;
-
 use std::time::Instant;
+use std::time::Duration;
 use std::fs::OpenOptions;
 use std::io::Write;
 
+use crossbeam_channel::unbounded;
+use std::{thread};
+
+use once_cell::sync::OnceCell;
+
+static CLIENT_KE_S: OnceCell<crossbeam_channel::Sender<u128>> = OnceCell::new();
+
 fn main() {
-    let mut f = OpenOptions::new()
+    // Create an unbounded channel
+    let (client_ke_s, client_ke_r) = unbounded();
+
+    CLIENT_KE_S.set(client_ke_s).expect("unable to fill once cell.");
+
+    // make a thread for writing client_ke meas
+    thread::spawn(move || {
+        let mut f = OpenOptions::new()
         .write(true)
         .append(true)
-        .open("res")
+        .open("results/client_nts_ke")
         .expect("Unable to create file");
 
-    let start = Instant::now();
+        loop {
+            // get and write measurements
+            let value = client_ke_r.recv().unwrap();
+            writeln!(f, "{}", value).expect("Unable to write file");
+        }
+    });
 
-    // 2x for start and end
-    let _bench = Instant::now();
-    let _bench = Instant::now();
 
-    let end = Instant::now();
+    for _ in 1..500 {
+        let start = Instant::now();
 
-    let time_meas_nanos = end - start;
+        // 2x for start and end
+        let _bench = Instant::now();
+        let _bench = Instant::now();
 
-    writeln!(f, "{}", time_meas_nanos.as_nanos()).expect("Unable to write file");
-    // println!("{}", time_meas_nanos.as_nanos());
+        let end = Instant::now();
 
-    // f.write_all(&time_meas_nanos.to_be_bytes()).expect
-    
+        let time_meas_nanos = (end - start).as_nanos();
+
+        CLIENT_KE_S.get().clone().unwrap().send(time_meas_nanos).expect("unable to write to channel.");
+    }
+
+    // give the writer a chance to write before exiting (mimics real code)
+    thread::sleep(Duration::from_millis(1000));
 }
 
